014a1924c782756d342dd0da65e950dc746792af
geekture <nathanbernardo1233@gmail.com>
2020-05-02 03:47:43 -0400

    Update end.js

--- js/end.js
+++ js/end.js
@@ -24,5 +24,5 @@ saveHighScore = e => {
     highScores.sort( (a, b) => b.score - a.score);
     highScores.splice(5);
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
+    window.location.assign('/quiz-bible');
 };
