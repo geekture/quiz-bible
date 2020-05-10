ca8ef68ef750dcbc8d79fd567b16aabd8c1bdc9f
geekture <nathanbernardo1233@gmail.com>
2020-05-10 02:42:39 -0400

    Delete highscores.js

--- js/highscores.js
+++
@@ -1,5 +0,0 @@
-const highScoresList = document.getElementById('highScoresList');
-const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-highScoresList.innerHTML = highScores.map(score => {
-    return `<li class="high-score"><b>${score.name} : </b>${score.score} points</li>`;
-}).join("");
