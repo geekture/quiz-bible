6873c8c90aebea7b47ba07d324328926472cd375
geekture <nathanbernardo1233@gmail.com>
2020-05-10 03:11:07 -0400

    Update app-info.js

--- js/app-info.js
+++ js/app-info.js
@@ -4,7 +4,7 @@ shareBtn.addEventListener('click', () => {
         navigator.share({
             title: 'QuizBibleApp',
             text: 'Découvrez QuizBibleApp, je l\'utilise pour tester et améliorer mes connaissances bibliques. Téléchargez-la gratuitement à',
-            url: 'https://geekture.github.io/quiz-biblique'
+            url: 'https://geekture.github.io/quiz-bible'
         })
         .then(() => console.log('OK'))
         .catch((error) => console.log('Erreur de partage', error));
