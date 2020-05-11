af68da2b540bbf077964d0f4b08be422bc4b00db
geekture <nathanbernardo1233@gmail.com>
2020-05-11 15:28:04 -0400

    Update register-sw.js

--- js/register-sw.js
+++ js/register-sw.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('js/service-worker.js');
+        navigator.serviceWorker.register('/js/service-worker.js');
     });
 }
